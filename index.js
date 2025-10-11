42516fa90ad14d74d5a99f5cfe380dac86427c5d
Jonatan Góngora <jonatan.gongora1988@gmail.com>
2025-10-11 20:45:13 -0300

    GET products/{id}

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ let [, , method, resource] = process.argv;
 method = method.toUpperCase();
 resource = resource.toLowerCase();
 
+if (method == "GET" && resource.startsWith("products/")) {
+    let id = resource.split("/")[1];
+    id = parseInt(id);
+
+    if (isNaN(id) || id <= 0) {
+        console.log("No es un número");
+    }
+
+    fetch("https://fakestoreapi.com/products/" + id)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+}
+
 if (method == "GET" && resource == "products") {
 const response = await fetch('https://fakestoreapi.com/products')
   const data = await response.json();
